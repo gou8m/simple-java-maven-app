c94f2f2bd8ae8f44200113bef2c534fdc31d7b63
Goutham Kumar Aare <gouthamreddy671@gmail.com>
2025-05-13 08:26:58 +0530

    Update App.java

--- src/main/java/com/project/App.java
+++ src/main/java/com/project/App.java
@@ -1,18 +1,39 @@
 package com.project;
 
-import java.util.ResourceBundle;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class App {
 
     public int userLogin(String in_user, String in_pwd) {
-        ResourceBundle rb = ResourceBundle.getBundle("config");
-        String username = rb.getString("username");
-        String password = rb.getString("password");
+        Properties props = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream("conf/config.properties");
+            props.load(fis);
+        } catch (IOException e) {
+            System.out.println("Could not load config: " + e.getMessage());
+            return 0;
+        }
+
+        String username = props.getProperty("username");
+        String password = props.getProperty("password");
 
         if (in_user.equals(username) && in_pwd.equals(password)) {
             return 1;
         } else {
             return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        int result = app.userLogin("abc", "abc@123");
+
+        if (result == 1) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Login failed.");
         }
     }
 }
